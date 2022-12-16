ef547683fe47471c10d092d89535df472c5e11d9
MarcosASilva <marcos33112@gmail.com>
2022-12-16 15:00:24 -0300

    Commit atualização

---
+++ funcionamento.js
@@ -0,0 +1,29 @@
+function log() {
+    alert("MEU LORO PARAGUAIO")
+    // window.document.getElementById(visor)
+};
+
+
+function clickbotao(clicar) {
+    let res = 0
+    if (clicar == 0){
+        b0 = 0 ;
+        res = res + 0;
+        alert(res)
+        } else if (clicar == "1") {
+            b1 = 1;
+            res = res + b1;
+            alert(res)
+            } else if (clicar == "2") {
+                b2 = 2;
+                res = res + b2;
+                alert(res)
+                } else if (clicar == "3") {
+                    b3 = 3;
+                    res = res + b3;
+                    } else if (clicar == "=") {
+                        alert(res);
+                        } else {
+
+    }
+}
